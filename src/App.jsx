3a52bd7534b178d1f82cd6511d998c6ef92e323f
Erick Tolentino <ericktolentino@pursuit.org>
2023-10-23 08:48:51 -0400

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import Index from "./pages/Index";
 import Show from "./pages/Show";
 import New from "./pages/New";
